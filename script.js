cc8a5822d4f5082d09bfaeaa0c8f68a63891980f
Gustavo Mattos <gustavomattos777@gmail.com>
2022-09-27 21:18:36 -0300

    inserindo vanilla tilt js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false'
+const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=40&page=1&sparkline=false'
 
 async function fetchApi() {
     resposta = await fetch(url)
@@ -7,15 +7,18 @@ async function fetchApi() {
     console.log(data)
 
     function cripto() {
-        let content = document.querySelector('#conteudo')
+        let content = document.querySelector('#container')
         data.forEach((coin, i) => {
             let img = data[i].image
+            let key = data[i].id
             content.innerHTML += `
-        <div class="conteudo">
+        <div class="card">
+        <div class="frontside" id=${key}>
            <h1>${data[i].name}</h1>
            <h1>${data[i].current_price}</h1>
            <h1 id="price">${data[i].price_change_percentage_24h.toFixed(2)}% </h1>
            <img src="${img}" alt="cripto" />
+        </div>
         </div>
         `
             let variacao = document.querySelector('#price')
@@ -27,8 +30,28 @@ async function fetchApi() {
                 variacao.setAttribute('id', 'green')
             }
 
-            console.log(content)
-        })
+            document.addEventListener('click', function (e) {
+                const el = e.target;
+                console.log('el', el)
+                if (el.classList.contains('frontside')) {
+                    el.classList.toggle('flip')
+                }
+            })
+
+            document.addEventListener('mouseover', function (e) {
+                const element = e.target;
+                console.log('el', element)
+                if (element.classList.contains('frontside')) {
+                    VanillaTilt.init(element, {
+                        max: 25,
+                        speed: 400,
+                        glare: true,
+                        "max-glare": 1,
+                    });
+                }
+            })
+
+       })
     }
 
     cripto();
